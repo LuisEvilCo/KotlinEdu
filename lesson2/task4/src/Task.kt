cb89ddf903ea3022938ca9af98ed62a825bf1745
luis <ltm150895@gmail.com>
2017-03-15 16:56:35 -0600

    Level 2 complete

--- lesson2/task4/src/Task.kt
+++ lesson2/task4/src/Task.kt
@@ -1,4 +1,17 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(var start: MyDate, val end: MyDate): Iterator<MyDate> {
+    override fun hasNext(): Boolean {
+        return start < end.nextDay()
+    }
+
+    override fun next(): MyDate {
+        if(this.hasNext()){
+            var temp = start
+            start = start.nextDay()
+            return temp
+        }
+        throw NoSuchElementException()
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
--- lesson2/task5/src/Task.kt
+++ lesson2/task5/src/Task.kt
@@ -2,14 +2,24 @@ import TimeInterval.*
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
+data class IntervalQuantity(val count: Int, val interval: TimeInterval)
+
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(other: IntervalQuantity): MyDate {
+    return addTimeIntervals(other.interval, other.count)
+}
+
+operator fun TimeInterval.times(number: Int): IntervalQuantity {
+    return IntervalQuantity(number, this)
+}
 
 fun task1(today: MyDate): MyDate {
-    return today + YEAR + WEEK
+    return today +  YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
+
+
